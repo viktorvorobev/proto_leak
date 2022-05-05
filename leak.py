17a03c62b01a4a46d1011f4c6c4ae92f4ecc66b0
Lasse Vang Gravesen <lasg@saxobank.com>
2022-05-05 14:51:47 +0200

    feat: replace with own code.

--- leak.py
+++ leak.py
@@ -1,36 +1,29 @@
 import psutil
 import os
-from foo_pb2 import Foo
+import datetime
+from schema_pb2 import value_test_topic
 
 process = psutil.Process(os.getpid())
 
-def get_vms_MB():
-    return (process.memory_full_info().vms / 1024) / 1024
+def get_rss_MB():
+    return (process.memory_full_info().rss / 1024) / 1024
 
-print("Memory consumed at the beginning", get_vms_MB())
+print("Memory consumed at the beginning", get_rss_MB())
 
-with open("buffer", "rb") as fd:
-    foo = Foo()
-    foo.ParseFromString(fd.read())
+lst = []
+for _ in range(1000000):
+    event_dt = datetime.datetime.utcnow()
+    lst.append(value_test_topic(
+        myField1=1234567,
+        myField2=event_dt.timestamp(),
+        myField3="abc " * 100
+    ))
 
-print("Memory consumed after parsed", get_vms_MB())
+print("Memory consumed after parsed", get_rss_MB())
 
-del(foo)
-print("Memory consumed after deallocating", get_vms_MB())
+while len(lst):
+    lst.pop()
 
+del lst
 
-with open("buffer", "rb") as fd:
-    foo = Foo()
-    foo.ParseFromString(fd.read())
-
-print("Memory consumed after parsed", get_vms_MB())
-
-for bar in foo.bar:
-    for x in bar.x:
-        pass
-
-print("Memory consumed after iterating", get_vms_MB())
-
-del(foo)
-
-print("Memory consumed after deallocating", get_vms_MB())
+print("Memory consumed after deallocating", get_rss_MB())
