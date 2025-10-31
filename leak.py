33ec9968bec2c3618842ec482aabc6e9ff9bd2cc
Viktor Vorobev <vikeyer@gmail.com>
2025-10-31 12:00:00 +0100

    leak: update script to show leak while object is not deleted

--- leak.py
+++ leak.py
@@ -1,29 +1,27 @@
-import psutil
 import os
-import datetime
-from schema_pb2 import value_test_topic
+
+import psutil
+
+import schema_pb2
 
 process = psutil.Process(os.getpid())
+
 
 def get_rss_MB():
     return (process.memory_full_info().rss / 1024) / 1024
 
+
 print("Memory consumed at the beginning", get_rss_MB())
 
-lst = []
-for _ in range(1000000):
-    event_dt = datetime.datetime.utcnow()
-    lst.append(value_test_topic(
-        myField1=1234567,
-        myField2=event_dt.timestamp(),
-        myField3="abc " * 100
-    ))
+obj = schema_pb2.value_test_topic()
 
-print("Memory consumed after parsed", get_rss_MB())
+for _ in range(10_000_000):
+    obj.ParseFromString(
+        b"\x08\x87\xadK\x11\x00\x00\x00\n.7\xd8A\x1a\x90\x03abc abc abc abc abc abc abc abc abc abc abc abc abc abc abc abc abc abc abc abc abc abc abc abc abc abc abc abc abc abc abc abc abc abc abc abc abc abc abc abc abc abc abc abc abc abc abc abc abc abc abc abc abc abc abc abc abc abc abc abc abc abc abc abc abc abc abc abc abc abc abc abc abc abc abc abc abc abc abc abc abc abc abc abc abc abc abc abc abc abc abc abc abc abc abc abc abc abc abc abc "
+    )
 
-while len(lst):
-    lst.pop()
+print("Memory consumed after parsing", get_rss_MB())
 
-del lst
+del obj
 
 print("Memory consumed after deallocating", get_rss_MB())
